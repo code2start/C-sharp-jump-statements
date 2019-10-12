617b93fa3bd446ff665bd1eb245d9f16e71a2181
Mohamed Fathy <codetwostart@gmail.com>
2019-10-12 18:00:22 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace jumpstatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == 2)
+                    //break
+                    //return;
+                    //continue;
+                    goto x;
+                Console.WriteLine($"Item number {i}");
+            }
+            Console.WriteLine("In the main method");
+            x://label
+            Console.WriteLine("I'm gotto keyword");
+        }
+    }
+}
